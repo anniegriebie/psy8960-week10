78c07a17c75436d0dbf1d6810a2272bfd7d132e7
anniegriebie <GRIEB046@umn.edu>
2023-03-31 13:58:29 -0500

    Models running, something weird going on with my premade calls, need to come back to either fix them or delete

--- R/week10.R
+++ R/week10.R
@@ -15,6 +15,7 @@ gss_tbl <- gss_tbl[, which(colMeans(!is.na(gss_tbl)) >= 0.75)] %>%
 mutate_all(as.numeric)
 
 
+
 ##Visualization 
 #adding visualization
 ggplot(gss_tbl, aes(x = workhours)) +
@@ -26,27 +27,34 @@ ggplot(gss_tbl, aes(x = workhours)) +
 #set seed for reproducibility
 set.seed(331)
 
+#creating train and holdout samples using technique demonstrated in DataCamp
+rows <- sample(nrow(gss_tbl))
+shuffled_data <- gss_tbl[rows,]
+split <-round(nrow(shuffled_data) *0.75)
+train_gss_tbl <- shuffled_data[1:split, ] %>%
+  mutate_all(as.numeric)
+test_gss_tbl <- shuffled_data[(split +1): nrow(shuffled_data),] 
+
 #creating a training vector to do 
 #10 folds
-index <- createFolds(train_gss_tbl$workhours, k=10)
-train_gss_tbl <-
-test_gss_tbl <-
+folds <- createFolds(train_gss_tbl$workhours, 10)
+#is this the right thing to use, should be using a different fold function?
 
-  
 #Creating gss_control to input into trControl for each model so don't have to redo it each time 
-gss_control <- trainControl(method= "cv", number = "10", search = "grid", verboseIter = T, indexOut = index)
+gss_control <- trainControl(method="cv", indexOut = folds, number = 10, search = "grid", verboseIter=T) # For some reason models wont run with gss_control variable come back and figure out
 
 #Creating gss_process to input into preProcess for each model so don't have to redo it each time 
-gss_process <- c("medianImpute","nzv", "center", "scale")
+gss_process <- c("medianImpute","nzv", "center", "scale") #also not working so manually added, delete this if cant get to work. 
 
 #OLS ready
 OLS <- train(
   workhours ~ .,
   train_gss_tbl,
-  method ="lm",
-  preProcess = gss_process,
-  na.acton = na.pass,
-  trControl = gss_control)
+  method = "lm",
+  na.action = na.pass,
+  preProcess = "medianImpute",
+  trControl = trainControl(method="cv", indexOut = folds, number = 10, search = "grid", verboseIter=T)
+)
 summary(OLS)
 
 #Elastic Net
@@ -55,9 +63,10 @@ ElasticNet <-
     workhours ~ .,
     train_gss_tbl, 
     method = "glmnet",
-    preProcess = gss_process,
     na.action = na.pass,
-    trControl = gss_control)
+    preProcess = "medianImpute",
+    trControl = trainControl(method="cv", indexOut = folds, number = 10, search = "grid", verboseIter=T)
+  )
 summary(ElasticNet)
 
 #Random forest
@@ -66,10 +75,11 @@ RandomForest <-
     workhours ~ .,
     train_gss_tbl, 
     method = "ranger",
-    preProcess = gss_process,
     na.action = na.pass,
-    trControl = gss_control)
-summary(RandomForeset)
+    preProcess = "medianImpute",
+    trControl = trainControl(method="cv", indexOut = folds, number = 10, search = "grid", verboseIter=T)
+  )
+summary(RandomForest)
 
 
 #XGBoost
@@ -78,9 +88,10 @@ boost <-
     workhours ~ .,
     train_gss_tbl, 
     method = "xgbTree",
-    preProcess = gss_process,
     na.action = na.pass,
-    trControl = gss_control)
+    preProcess = "medianImpute",
+    trControl = trainControl(method="cv", indexOut = folds, number = 10, search = "grid", verboseIter=T)
+  )
 summary(boost)
 
 #Publication
