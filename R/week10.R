ae53f4efc025621ca362a06a81aacc19f6b654da
anniegriebie <GRIEB046@umn.edu>
2023-03-31 11:33:15 -0500

    Cleaning data frame

--- R/week10.R
+++ R/week10.R
@@ -9,4 +9,8 @@ set.seed(331)
 #reads in data, renames HRS1 to workhours, and removes anyone who has a missing value for workhours 
 gss_tbl <- read_sav("../data/GSS2016.sav")%>%
   rename(workhours = HRS1) %>%
-  filter(complete.cases(workhours)) 
+  filter(complete.cases(workhours))
+
+#removing variables with equal to or more than 75% missingness, variables are columns so subset columns
+gss_tbl <- gss_tbl[, which(colMeans(!is.na(gss_tbl)) >= 0.75)] %>%
+mutate_all(as.numeric)
