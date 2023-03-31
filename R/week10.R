f4fa72a271873e4e587f1cf4c68abd235a3ba6b7
anniegriebie <GRIEB046@umn.edu>
2023-03-31 11:14:54 -0500

    renaming workhours and filtering complete cases

--- R/week10.R
+++ R/week10.R
@@ -5,5 +5,8 @@ library(haven)
 library(caret)
 set.seed(331)
 
-# Data Import and Cleaning
-gss_tbl <- read_sav("../data/GSS2016.sav")
+## Data Import and Cleaning
+#reads in data, renames HRS1 to workhours, and removes anyone who has a missing value for workhours 
+gss_tbl <- read_sav("../data/GSS2016.sav")%>%
+  rename(workhours = HRS1) %>%
+  filter(complete.cases(workhours)) 
